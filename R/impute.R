8e308b44d54d5d33f1bc2a256fdaa53811106e2e
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-06-10 16:49:58 +0100

    Added imputation fuction

---
+++ R/impute.R
@@ -0,0 +1,34 @@
+impute.missing <- function(X,bp=1:ncol(X),strata=NULL, numeric=FALSE) {
+  N <- as(X,"numeric")
+  if(!is.null(strata)) {
+    strata <- as.factor(strata)
+    if(length(levels(strata))>10)
+      stop("too many levels in strata\n")
+    for(i in levels(strata)) {
+      cat("\nstrata",i,"\n")
+      wh <- which(strata==i)
+      N[wh,] <- impute.missing(X[wh,,drop=FALSE],bp, numeric=TRUE)
+    }
+  } else {
+    csumm <- col.summary(X)
+    wh <- which(csumm[,"Call.rate"]<1)
+    cat(length(wh),"to impute\n")
+    for(i in wh) {
+      cat(i,".")
+      rule <- snp.imputation(X[,-i,drop=FALSE],X[,i,drop=FALSE],bp[-i],bp[i])
+      if(is.null(rule@.Data[[1]]))
+        next
+      imp <- impute.snps(rule,X[,-i,drop=FALSE])
+      wh.na <- which(is.na(N[,i]))
+      N[wh.na,i] <- imp[wh.na]
+    }
+    cat("\n")
+  }
+  if(numeric) {
+    return(N)
+  } else {
+    return(new("SnpMatrix",
+               data=round(N)+1, nrow=nrow(N), ncol=ncol(N), dimnames=dimnames(N)))
+  }
+}
+
